ac77af0b92a73b12fd8bc0e350f71bac89c4ca86
vinay Joshi <139681680+VinayJoshi16@users.noreply.github.com>
2025-12-08 04:57:00 +0000

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LearningProvider } from "@/contexts/LearningContext";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { ModuleProgressProvider } from "@/contexts/ModuleProgressContext";
 import { AppLayout } from "@/components/layout/AppLayout";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
 import Home from "./pages/Home";
@@ -21,32 +22,34 @@ const App = () => (
     <TooltipProvider>
       <AuthProvider>
         <LearningProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <AppLayout>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/auth" element={<Auth />} />
-                <Route path="/learn" element={
-                  <ProtectedRoute>
-                    <Learn />
-                  </ProtectedRoute>
-                } />
-                <Route path="/quiz" element={
-                  <ProtectedRoute>
-                    <Quiz />
-                  </ProtectedRoute>
-                } />
-                <Route path="/dashboard" element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                } />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </AppLayout>
-          </BrowserRouter>
+          <ModuleProgressProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <AppLayout>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/auth" element={<Auth />} />
+                  <Route path="/learn" element={
+                    <ProtectedRoute>
+                      <Learn />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="/quiz" element={
+                    <ProtectedRoute>
+                      <Quiz />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="/dashboard" element={
+                    <ProtectedRoute>
+                      <Dashboard />
+                    </ProtectedRoute>
+                  } />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </AppLayout>
+            </BrowserRouter>
+          </ModuleProgressProvider>
         </LearningProvider>
       </AuthProvider>
     </TooltipProvider>
